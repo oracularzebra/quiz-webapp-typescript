74704eb8b0d799cf2f1eca75aa69ecbf9ce35b5d
onion <onion@orange.com>
2023-10-13 20:13:19 +0000

    added hi

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -15,6 +15,7 @@ export default function Home({username, loggedIn}:UserProps){
     }, [loggedIn]);
     return (
         <>
+            hello {username}
             <Categories/>
             <PreviousAttempts/>
         </>
