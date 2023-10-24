4ba9548e9255587e9a429ebf98e83dd8cf4bf32d
onion <onion@orange.com>
2023-10-24 14:35:38 +0000

    Added styling

--- src/routes/attempts/PreviousAttempts.tsx
+++ src/routes/attempts/PreviousAttempts.tsx
@@ -21,22 +21,26 @@ export default function PreviousAttempts(props: PreviousAttemptsProps){
         })()
     }, [])
     return (
-        <>
+        <div className="flex flex-wrap place-content-center">
             {attempts != null ?attempts.map((attempt)=>{
                 return (
-                    <>
-                        <button key={attempt.attempt_id} onClick={()=>{
+                    <div 
+                    key={attempt.attempt_id}
+                    className="">
+                        <button
+                        className="bg-slate-400 rounded-lg p-2 m-1" 
+                        onClick={()=>{
                             navigate(`/attempt`, 
                             {state: attempt});
                         }}>
-                        {`${attempt.attempt_date.slice(0, 15)}
+                        {`${attempt.attempt_date.slice(0, 21)}
                         ${attempt.category}:${attempt.difficulty}`}
                         </button>
-                    </>
+                    </div>
                 )
             })
             :
             <>loading</>}
-        </>
+        </div>
     )
 }
--- src/routes/attempts/SingleAttempt.tsx
+++ src/routes/attempts/SingleAttempt.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom"
 import { getAttempt } from "./previousAttempts";
 import { ResultReq, ResultRes } from "../result/result";
 
 export default function SingleAttempt(){
 
+    const navigate = useNavigate();
     const req:ResultReq = useLocation().state;
     const [result, setResult] = useState<ResultRes | null>(null);
     useEffect(()=>{
@@ -15,32 +16,53 @@ export default function SingleAttempt(){
             setResult(res);
         })()
     }, [])
-    return (
-        <>
-            {result == null
+    return ( 
+    <div className="grid">
+            {
+                result == null
                 ?
                 <>loading...</>
                 :
                 <>
-                {result.category}:{result.difficulty}                
-                marks 
-                {result.marks}
+                <h2 className="text-center text-lg">
+                {result!.category}:{result!.difficulty}                
                 <br />
-                {result.duration}
-                <ul style={{listStyle:'none'}}>
-                    <li style={{backgroundColor:"green"}}>correct</li>
-                    <li style={{backgroundColor:"orange"}}>Attempted and correct</li>
-                    <li style={{backgroundColor:"red"}}>Attempted and wrong</li>
+                Marks:
+                {result!.marks}/{result.questions.data.length}
+                <br />
+                Time Taken:
+                {result.duration.min}min
+                {result.duration.sec}sec,
+                Total Duration:
+                {result.questions.data.length-1}min
+                59sec
+                </h2>
+                <ul
+                className="flex place-content-center gap-2 m-5" 
+                style={{listStyle:'none'}}>
+                    <li
+                    className="p-1 rounded-xl" 
+                    style={{backgroundColor:"green"}}>correct</li>
+                    <li 
+                    className="p-1 rounded-xl" 
+                    style={{backgroundColor:"orange"}}>Attempted and correct</li>
+                    <li
+                    className="p-1 rounded-xl" 
+                    style={{backgroundColor:"red"}}>Attempted and wrong</li>
                 </ul>
                 {result.questions.data.map((obj, index1)=>
-                    (<div key={obj.id}>
+                    (<div
+                    className="text-lg"
+                    key={obj.id}>
                         Q{index1+1}{obj.question}
                         <ul style={{listStyle:'none'}}>
                         {obj.options.map((op, index2)=>
                             {
                             const correct_answer = result.correct_answers[index1];
                             const marked_option = req.marked_options[index1];
-                            return <li key={index2} style={{
+                            return <li
+                                className="p-2"
+                                key={index2} style={{
                                 color: op == correct_answer ? 
                                 correct_answer == marked_option ? 'yellow' : 'green' :
                                 marked_option == op ? 'red' 
@@ -54,6 +76,18 @@ export default function SingleAttempt(){
                 )}
                 </>
             }
-        </>
+            <div 
+            className="place-self-center">
+              <button
+              className="bg-slate-400 rounded p-2 m-1"
+              onClick={()=>navigate(`/test/${result?.category}/${result?.difficulty}`)}
+              >Retake Test</button>
+              <button
+              className="bg-slate-400 rounded p-2 m-1"
+              onClick={()=>navigate('/home')}>Return to home</button>
+            </div>
+        </div>
     )
+    
+
 }
