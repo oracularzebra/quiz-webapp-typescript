2edc0c13425dc31cde2eb3fa9baa343e613dfd79
onion <onion@orange.com>
2023-10-24 14:24:15 +0000

    Added stying and retake test button

--- src/routes/result/Result.tsx
+++ src/routes/result/Result.tsx
@@ -6,9 +6,6 @@ export default function Result(){
 
     const location = useLocation();
     const navigate = useNavigate();
-    //here we need questions_id because we
-    //would use the same page to show previous 
-    //attempts.
     const {loggedIn} = location.state;
     const req:ResultReq = location.state;
     const [result, setResult] = useState<ResultRes | null>(null);
@@ -24,7 +21,6 @@ export default function Result(){
 
     useEffect(()=>{
         (async function(){
-            // const questions_id = questions?.data.map((obj:{id:number}) => obj.id);
             const result = await getResult(req);
             setResult(result);
             console.log(result);
@@ -32,33 +28,52 @@ export default function Result(){
     }, []);
 
     return (
-        <>
+        <div className="grid">
             {
                 result == null
                 ?
                 <>loading...</>
                 :
                 <>
+                <h2 className="text-center text-lg">
                 {result!.category}:{result!.difficulty}                
-                marks 
-                {result!.marks}
                 <br />
+                Marks:
+                {result!.marks}/{result.questions.data.length}
+                <br />
+                Time Taken:
                 {result.duration.min}min
-                {result.duration.sec}sec
-                <ul style={{listStyle:'none'}}>
-                    <li style={{backgroundColor:"green"}}>correct</li>
-                    <li style={{backgroundColor:"orange"}}>Attempted and correct</li>
-                    <li style={{backgroundColor:"red"}}>Attempted and wrong</li>
+                {result.duration.sec}sec,
+                Total Duration:
+                {result.questions.data.length-1}min
+                59sec
+                </h2>
+                <ul
+                className="flex place-content-center gap-2 m-5" 
+                style={{listStyle:'none'}}>
+                    <li
+                    className="p-1 rounded-xl" 
+                    style={{backgroundColor:"green"}}>correct</li>
+                    <li 
+                    className="p-1 rounded-xl" 
+                    style={{backgroundColor:"orange"}}>Attempted and correct</li>
+                    <li
+                    className="p-1 rounded-xl" 
+                    style={{backgroundColor:"red"}}>Attempted and wrong</li>
                 </ul>
                 {result.questions.data.map((obj, index1)=>
-                    (<div key={obj.id}>
+                    (<div
+                    className="text-lg"
+                    key={obj.id}>
                         Q{index1+1}{obj.question}
                         <ul style={{listStyle:'none'}}>
                         {obj.options.map((op, index2)=>
                             {
                             const correct_answer = result.correct_answers[index1];
                             const marked_option = req.marked_options[index1];
-                            return <li key={index2} style={{
+                            return <li
+                                className="p-2"
+                                key={index2} style={{
                                 color: op == correct_answer ? 
                                 correct_answer == marked_option ? 'yellow' : 'green' :
                                 marked_option == op ? 'red' 
@@ -72,7 +87,16 @@ export default function Result(){
                 )}
                 </>
             }
-            <button onClick={()=>navigate('/home')}>Return to home</button>
-        </>
+            <div 
+            className="place-self-center">
+              <button
+              className="bg-slate-400 rounded p-2 m-1"
+              onClick={()=>navigate(`/test/${result?.category}/${result?.difficulty}`)}
+              >Retake Test</button>
+              <button
+              className="bg-slate-400 rounded p-2 m-1"
+              onClick={()=>navigate('/home')}>Return to home</button>
+            </div>
+        </div>
     )
 }
