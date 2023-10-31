1feb24e5752c9b2ac73bdace269f54e1337910cb
onion <onion@orange.com>
2023-10-31 19:41:28 +0000

    added checking of username state

--- src/routes/admin/DashBoard.tsx
+++ src/routes/admin/DashBoard.tsx
@@ -1,10 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+import { backendUrl } from "../../App";
+
 export function AdminDashBoard(){
 
-  
+  const [noOfUsers, setNoOfUsers] = useState<number>(0);
+  useEffect(()=>{
+    (async()=>{
+      const noOfUsersReq = await axios({
+        url:`${backendUrl}/admin/get-users`
+      });
+      //Here we are getting a full blown object
+      //involving all the headers, data obj only is 
+      //of need here
+      setNoOfUsers(noOfUsersReq.data.data);
+    })();
+  }, [])
   
   return (
-    <>
-      dashboard
-    </>
+    <div>
+      <h1 className="text-center text-xl m-2">
+        Admin dashboard
+      </h1>
+      Total No of users {noOfUsers}
+    </div>
   )
 }
